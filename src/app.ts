5fa8afb8c7311cd79e44301b0a90ac0851caa70a
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-06-19 23:23:20 +0600

    default response added

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import router from "./app/routes";
 import globalErrorHandler from "./app/middlewares/globalErrorhandler";
@@ -17,4 +17,7 @@ app.get("/api/check-availability", bookingController.checkAvailableSlot);
 
 //global error handler
 app.use(globalErrorHandler);
+app.get("/", (req: Request, res: Response) => {
+  res.send("Booking Server Running");
+});
 export default app;
