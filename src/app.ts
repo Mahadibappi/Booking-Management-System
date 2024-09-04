5974d48411d7e53d768a1dd7da91144cf803acee
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-09-04 09:30:45 +0600

    login function added

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,11 @@ const app: Application = express();
 
 //middlewares
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 //routes
 app.use("/api", router);
