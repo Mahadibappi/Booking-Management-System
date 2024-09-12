ffa9db0556816e7de67caa2c05fc25a5b510c38c
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2024-09-12 10:48:30 +0600

    multer added

--- src/app/modules/facility/facility.controller.ts
+++ src/app/modules/facility/facility.controller.ts
@@ -3,18 +3,53 @@ import { facilityService } from "./facility.service";
 import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import sendResponse from "../../utils/sendResponse";
+import { v2 as cloudinary } from "cloudinary";
+import { Facility } from "./facility.model";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: "cloud-mahadi",
+  api_key: "628425217641132",
+  api_secret: "RNTnDYsj8FjLKJUSygvr4v4TlhA",
+  secure: true,
+});
 
 const createFacility = catchAsync(async (req: Request, res: Response) => {
-  const facility = req.body;
+  try {
+    const { name, description, pricePerHour, location } = req.body;
 
-  const result = await facilityService.createFacilityIntoDb(facility);
+    let imageUrl = "";
 
-  sendResponse(res, {
-    statusCode: httpStatus.OK,
-    success: true,
-    message: "Facility Created Successfully",
-    data: result,
-  });
+    if (req.file) {
+      try {
+        const result = await cloudinary.uploader.upload(req.file.path, {
+          folder: "facilities",
+        });
+        imageUrl = result.secure_url;
+        fs.unlinkSync(req.file.path);
+      } catch (error) {
+        console.error("Cloudinary upload error:", error);
+        return res.status(500).json({ error: "Cloudinary upload failed" });
+      }
+    }
+
+    const facilityData = {
+      name,
+      description,
+      pricePerHour: Number(pricePerHour),
+      location,
+      image: imageUrl,
+    };
+
+    const facility = await Facility.create(facilityData);
+    console.log(facility);
+    res.status(201).json({
+      success: true,
+      data: facility,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 const getAllFacility = catchAsync(async (req: Request, res: Response) => {
